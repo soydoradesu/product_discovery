35a5e5bb97d2ae8a22ab27accb0d596929cdee9a
soydoradesu <darwinisfish@gmail.com>
2026-01-12 22:43:04 +0700

    chore: init backend

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
+	http.ListenAndServe(":8080", nil)
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/soydoradesu/product_discovery
+
+go 1.25.1
